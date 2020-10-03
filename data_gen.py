9587705da255a4669ef9959774ccb11db38e7dcd
sahibpreet <ss9334931@gmail.com>
2020-10-04 01:13:30 +0530

    commenting on data_gen

--- data_gen.py
+++ data_gen.py
@@ -3,18 +3,20 @@ import numpy as np
 
 
 
-n_samples=[100,200,300]
+n_samples=[100,200,300] # number of samples
 
-feature_list=[2,3,4]
+feature_list=[2,3,4] # features
 
-datasets= []
+datasets= [] # list containing all the genrated datasets
 
 dataset_creator=[make_circles,make_moons,make_swiss_roll]
 
+# diff loop because make_blobs takes centres argument where others do not
 for iter,features in zip(n_samples,feature_list):
     X,y = make_blobs(n_samples=iter,n_features=features,centers=3)
     datasets.append(X)  # appending datasets genrated from make_blobs
 
+# iterator for creating diffrent datasets from sklearn
 for creator in dataset_creator:
     for iter in n_samples:
         X,y=creator(n_samples=iter)
