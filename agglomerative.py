61c4b7c24ed4c007fbb08279d5054a92c91b41bb
sahibpreet <ss9334931@gmail.com>
2020-10-07 11:29:34 +0530

    making use of euclidean function defined in clustering.py for  agglomertive clustering.py

--- agglomerative.py
+++ agglomerative.py
@@ -1,17 +1,18 @@
 from data_gen import datasets
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.cluster import AgglomerativeClustering
-
+from clustering import Clustering
+from numpy.linalg import norm
 arr=np.array([[1,1],[2,2],[1,3],[2,1]
             ,[9,9],[10,10],[8,8],
             [15,15],[14,15],[15,14]])
 
-sns.scatterplot(x=arr[:,0],y=arr[:,1],hue=arr.shape[1])
+agg_clustering=Clustering()
 
 distance_arr=np.zeros(shape=(arr.shape[0],arr.shape[0]))
 
-print(distance_arr)
-
+# for i in range(distance_arr.shape[0]):
+#     distance_arr[:,i]=get_distance()
+# print(norm(arr[0,:]-arr[9,:]))
+print(agg_clustering.get_distance(cen=arr[0,:],arr=arr[2,:],axis=0))
