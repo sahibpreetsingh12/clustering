5e2e01e633c41279881152f55a431f175b1a38b3
sahibpreet <ss9334931@gmail.com>
2020-10-09 00:20:18 +0530

    now will make function for proximity matrix

--- agglomerative.py
+++ agglomerative.py
@@ -54,23 +54,35 @@ list_of_cordinates=list(zip(min_[0],min_[1])) # finding list of coordinates that
 # in proximity matrix
 
  
-new_array=np.empty((0,arr.shape[1])) 
+new_array=np.empty((0,arr.shape[1]))  # empty array of shape 0,shape of arr[1] where arr is our dataset
+
+# list_of_coordinates responds to those pair of points that have least euclidean distances in proximity matrix
+# list_of_coordinates[0] responds to just first pair that had least euclidean distance
+# because initially we had k clusters after step2 we'll have k-1 clusters although there can be multiple points that 
+# form clusters because they have also least euclidean distance
+
 clustered_p_1=list_of_cordinates[0][0] 
 clustered_p_2=list_of_cordinates[0][1]
+cluster_1=[]
 print("cluster made for point ",clustered_p_1 ,"and ",clustered_p_2)
-
+cluster_1.append([clustered_p_1,clustered_p_2])
+print(cluster_1)
 
 actual_1=arr[clustered_p_1].reshape(1,2)
-print(actual_1)
-# arr[clustered_p_2])
+
+actual_2=arr[clustered_p_2].reshape(1,2)
+
+arr=np.delete(arr,[clustered_p_1,clustered_p_2],axis=0) # removing rows that were clustered from original array
+
+print("After deletion",arr.shape)
 
 print("#####")
-print(pd.DataFrame(distance_arr))
+# print(pd.DataFrame(distance_arr))
+ 
+new_array=np.append(new_array,actual_1,axis=0)
+new_array=np.concatenate((new_array,arr))
+print(new_array.shape)
 
 # Have to think how can we utilise our previous calculations
 # print(np.where(distance_arr[list_of_cordinates[0][0]] == infinite, 0, distance_arr[list_of_cordinates[0][0]]))
 # print(np.where(distance_arr[list_of_cordinates[0][1]] == infinite, 0, distance_arr[list_of_cordinates[0][1]]))
-
-arr = 
-new_array=np.append(new_array,np.array(actual_1),axis=0)
-print(new_array)
